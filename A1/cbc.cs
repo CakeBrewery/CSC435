1014b1e01bef6407e5784bee5931641345b14786
Samuel N <samuel.e.navarrete@gmail.com>
2014-05-27 20:26:02 -0700

    command line flags fixed

--- A1/cbc.cs
+++ A1/cbc.cs
@@ -7,13 +7,26 @@ public class test
 	{
 	  Parser parser = new Parser();
 
-	  parser.flg_token = true; 
+	  parser.flg_token = false;
+	  parser.flg_debug = false;
+
+	  for(int i = 1; i < args.Length; i++){
+	  	if(args[i] == "-tokens"){
+	  		parser.flg_token = true;
+	  		System.Console.WriteLine("Debug: ON");
+	  	}
+	  	if(args[i] == "-debug"){
+	  		parser.flg_debug = true; 
+	  		System.Console.WriteLine("Tokens: ON");
+	  	}
+	  }
 
 
 	  FileStream file = new FileStream(args[0], FileMode.Open);
 	  parser.openFile(file); 
 	  System.Console.WriteLine("File: " + args[0]);
 
+	  System.Console.WriteLine("");
 
 	  parser.Parse();
 
